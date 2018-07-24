f35039c6e382bcc45c9779e5d0febe5643f432e5
Yuanchun Shen <immr.shen@gmail.com>
2018-07-24 18:25:07 +0800

    Add overview and me

---
+++ js/overview.js
@@ -0,0 +1,7 @@
+function showNewPost() {
+    document.getElementById("new-post").style.display = "block";
+}
+
+function hideNewPost() {
+    document.getElementById("new-post").style.display = "none";
+}
